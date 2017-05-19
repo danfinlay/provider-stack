cd6f1928fcf40dd20ab6c5cac35b0370c4de62be
Dan Finlay <dan@danfinlay.com>
2017-05-19 11:11:22 -0700

    Mutator test passed

--- test/index.js
+++ test/index.js
@@ -23,4 +23,38 @@ test('a simple response subprovider', function (t) {
   })
 })
 
+test('a param mutating subprovider', function (t) {
+  const success = 'success!'
+  const failure = 'failure!'
 
+  const mutator = {
+    handleRequest (params, next) {
+      params.value = 2
+      return
+    }
+  }
+
+  const responder = {
+    handleRequest (params, next) {
+      switch (params.value) {
+        case 2:
+          return Promise.resolve(success)
+        default:
+          return Promise.resolve(failure)
+      }
+    }
+  }
+
+  const stack = new ProviderStack()
+  stack.push(mutator)
+  stack.push(responder)
+
+  stack.handleRequest({ value: 1 })
+  .then((response) => {
+    t.equal(response, success, 'returns success')
+    t.end()
+  })
+  .catch((reason) => {
+    t.ok(false, reason)
+  })
+})
